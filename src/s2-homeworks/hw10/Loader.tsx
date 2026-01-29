44fe74f9405f74c961df5f0aeb65e08fd2da5249
Anton-013 <antonchuvashovoren@mail.ru>
2026-01-30 00:11:31 +0300

    hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,35 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+            <svg width="90" height="90" viewBox="0 0 90 90">
+                {/* <!-- Градиент --> */}
+                <defs>
+                    <linearGradient id="myGradient">
+                        <stop offset="0%" stop-color="#512DE4" />
+                        <stop offset="100%" stop-color="#ffffff" />
+                    </linearGradient>
+                </defs>
+
+                {/* <!-- Анимированный круг --> */}
+                <circle cx="45" cy="45" r="40"
+                    stroke="url(#myGradient)"
+                    stroke-width="9"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-dasharray="150 101">
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 45 45"
+                        to="360 45 45"
+                        dur="1s"
+                        repeatCount="indefinite"
+                    />
+                </circle>
+            </svg>
+        </div>
+
+    )
+}
